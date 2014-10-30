01b4be5a3ce4a010e51b442b81a4ee5b66712ea0
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-10-29 21:27:01 -0400

    added database insert code, and update for contact

---
+++ Database/doctors/update.php
@@ -0,0 +1,28 @@
+<?php
+
+// Create connection
+$con=mysqli_connect("localhost","root","bitnami","health_alertdb");
+
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+
+$sql = "UPDATE doctors
+		SET doctorName = ".$_GET['doctorName'].",
+		 	doctorPhoneNumber = ".$_GET['doctorPhoneNumber'].", 
+		 	doctorAddress = ".$_GET['doctorAddress']."
+		WHERE doctor_id = ".$_GET['doctor_id'];
+
+
+if ($con->query($sql) == TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error;
+}
+
+mysqli_close($con);
+
+?>
