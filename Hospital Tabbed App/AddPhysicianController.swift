f48491aa879a1806d8743256ac08f006fc3c2bd8
Chinmay Patwardhan <chinnychin19@gmail.com>
2014-10-29 22:12:19 -0400

    separated doctors and other contacts. implemented adding doctors

---
+++ Hospital Tabbed App/AddPhysicianController.swift
@@ -0,0 +1,61 @@
+//
+//  AddPhysicianController.swift
+//  Hospital Tabbed App
+//
+//  Created by Chinmay Patwardhan on 10/29/14.
+//  Copyright (c) 2014 Chinmay Patwardhan. All rights reserved.
+//
+
+import UIKit
+
+class AddPhysicianController : UIViewController {
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var phoneNumberField: UITextField!
+    @IBOutlet weak var addressField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        saveButton.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+    }
+        
+    func buttonAction(sender:UIButton!)
+    {
+        var nameStr : String = nameField.text;
+        var phoneNumberStr : String = phoneNumberField.text;
+        var addressStr : String = addressField.text;
+        println(nameStr)
+        println(phoneNumberStr)
+        println(addressStr)
+        var url = "http://colab-sbx-211.oit.duke.edu/PHPDatabaseCalls/doctors/insert.php?doctorName='"+nameStr+"'&doctorPhoneNumber='"+phoneNumberStr+"'&doctorAddress='"+addressStr+"'";
+        println("dirty url:" + url);
+        url = cleanURLString(url)
+        println("clean url:" + url);
+        makeHTTPRequest(url)
+    }
+    
+    func cleanURLString(url : String) ->String {
+        var out : String = "";
+        for char in url {
+            if char == " " {
+                out += "%20"
+            } else {
+                out += "\(char)";
+            }
+        }
+        return out
+    }
+
+    func makeHTTPRequest(urlStringWithParameters : String) {
+        let url = NSURL(string: urlStringWithParameters)
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+            println(NSString(data: data, encoding: NSUTF8StringEncoding))
+            
+        }
+        task.resume()
+    }
+    
+}
+
