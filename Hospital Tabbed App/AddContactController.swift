2cbb9f6d315ae22c37d452063e76d3e3e132468d
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-11-18 07:12:38 -0500

    added the ablity to choose which notiication that the added contact should get using a button. editing insert api for contacts to also update notifications table

--- Hospital Tabbed App/AddContactController.swift
+++ Hospital Tabbed App/AddContactController.swift
@@ -13,6 +13,11 @@ class AddContactController : UIViewController {
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var phoneNumberField: UITextField!
+    var isStepHighLighted:Int = -1
+    var isBodyMassHighLighted:Int = -1
+    var isOxySelectHighLighted:Int = -1
+    var isHRHighLighted:Int = -1
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,19 +25,76 @@ class AddContactController : UIViewController {
         saveButton.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
     }
     
+    @IBAction func stepSelect(sender: UIButton) {
+        dispatch_async(dispatch_get_main_queue(), {
+            if (self.isStepHighLighted == -1) {
+                sender.highlighted = true;
+                self.isStepHighLighted = 0;
+            }else{
+                sender.highlighted = false;
+                self.isStepHighLighted = -1;
+            }
+        });
+    }
+    
+    @IBAction func bodyMassSelect(sender: UIButton) {
+        dispatch_async(dispatch_get_main_queue(), {
+            if (self.isBodyMassHighLighted == -1){
+                sender.highlighted = true;
+                self.isBodyMassHighLighted = 1;
+            }else{
+                sender.highlighted = false;
+                self.isBodyMassHighLighted = -1;
+            }
+        });
+    }
+    
+    @IBAction func OxySelect(sender: UIButton) {
+        dispatch_async(dispatch_get_main_queue(), {
+            if (self.isOxySelectHighLighted == -1){
+                sender.highlighted = true;
+                self.isOxySelectHighLighted = 2;
+            }else{
+                sender.highlighted = false;
+                self.isOxySelectHighLighted = -1;
+            }
+        });
+    }
+    
+    @IBAction func hrSelect(sender: UIButton) {
+        dispatch_async(dispatch_get_main_queue(), {
+            if (self.isHRHighLighted == -1){
+                sender.highlighted = true;
+                self.isHRHighLighted = 3;
+            }else{
+                sender.highlighted = false;
+                self.isHRHighLighted = -1;
+            }
+        });
+    }
+    
+    
     func buttonAction(sender:UIButton!)
     {
         var patientId = UIDevice.currentDevice().identifierForVendor.UUIDString;
         var nameStr : String = nameField.text;
         var phoneNumberStr : String = phoneNumberField.text;
+        let statIds = ["7","1","6","9"];
+        var statIdSelections = [self.isStepHighLighted,self.isBodyMassHighLighted,self.isOxySelectHighLighted,self.isHRHighLighted];
+        
         println(nameStr)
         println(phoneNumberStr)
+        for i in statIdSelections {
+            if( i != -1){
         
-        var url = "http://colab-sbx-211.oit.duke.edu/PHPDatabaseCalls/contacts/insert.php?patient_id='\(patientId)'&contactName='\(nameStr)'&contactPhoneNumber='\(phoneNumberStr)'";
-        println("dirty url:" + url);
-        url = StringHelper.cleanURLString(url)
-        println("clean url:" + url);
-        makeHTTPRequest(url)
+                var url = "http://colab-sbx-211.oit.duke.edu/PHPDatabaseCalls/contacts/insert.php?patient_id='\(patientId)'&contactName='\(nameStr)'&contactPhoneNumber='\(phoneNumberStr)'";
+                println("dirty url:" + url);
+                url = StringHelper.cleanURLString(url)
+                println("clean url:" + url);
+                makeHTTPRequest(url)
+                
+            }
+        }
     }
         
     func makeHTTPRequest(urlStringWithParameters : String) {
