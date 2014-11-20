d61c4720143430d2bb30a149c2c2504440c6638e
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-11-20 12:53:05 -0500

    fixed contact add

--- Hospital Tabbed App/AddContactController.swift
+++ Hospital Tabbed App/AddContactController.swift
@@ -13,97 +13,204 @@ class AddContactController : UIViewController {
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var phoneNumberField: UITextField!
-    var isStepHighLighted:Int = -1
-    var isBodyMassHighLighted:Int = -1
-    var isOxySelectHighLighted:Int = -1
-    var isHRHighLighted:Int = -1
+    
+    //tuple - (text, call)
+    var stepHighLightTuple = (text:-1,call:-1)
+    var bodyMassHighLightTuple = (text:-1,call:-1)
+    var oxyHighLightTuple = (text:-1,call:-1)
+    var hRHighLightTuple = (text:-1,call:-1)
+    
+//    var isStepTextHighLighted:Int = -1
+//    var isBodyMassTextHighLighted:Int = -1
+//    var isOxySelectTextHighLighted:Int = -1
+//    var isHRTextHighLighted:Int = -1
+//    
+//    var isStepCallHighLighted:Int = -1
+//    var isBodyMassCallHighLighted:Int = -1
+//    var isOxySelectCallHighLighted:Int = -1
+//    var isHRCallHighLighted:Int = -1
+    
+    var stepsNotificationTuple = (statId:7, isText: false, isCall: false)
+    var bodyMassNotificationTuple = (statId: 1,isText: false, isCall: false)
+    var oxyNotificaitonTuple = (statId: 6, isText: false,isCall: false)
+    var hrNotificaitionTuple = (statId: 9,isText: false,isCall: false)
+    
+    //tuple - (stat_id, text_on, call_on)
+    
+//    var isTextStep:Bool = false
+//    var isTextOxi:Bool = false
+//    var isTextHR:Bool = false
+//    var isTextBM:Bool = false
+//    
+//    var isCallStep:Bool = false
+//    var isCallOxi:Bool = false
+//    var isCallHR:Bool = false
+//    var isCallBM:Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-  
-
 
         saveButton.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
     }
     
+    //keyboard hiding after editing text field
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         self.view.endEditing(true)
     }
     
-    @IBAction func stepSelect(sender: UIButton) {
+    // button for enable / disable text
+    @IBAction func stepTextSelect(sender: UIButton) {
         dispatch_async(dispatch_get_main_queue(), {
-            if (self.isStepHighLighted == -1) {
+            if (self.stepHighLightTuple.text == -1) {
                 sender.highlighted = true;
-                self.isStepHighLighted = 0;
+                self.stepsNotificationTuple.isText = true;
+                self.stepHighLightTuple.text = 0;
             }else{
                 sender.highlighted = false;
-                self.isStepHighLighted = -1;
+                self.stepHighLightTuple.text = -1;
+                self.stepsNotificationTuple.isText = false;
             }
         });
     }
     
-    @IBAction func bodyMassSelect(sender: UIButton) {
+    @IBAction func bodyMassTextSelect(sender: UIButton) {
         dispatch_async(dispatch_get_main_queue(), {
-            if (self.isBodyMassHighLighted == -1){
+            if (self.bodyMassHighLightTuple.text == -1){
                 sender.highlighted = true;
-                self.isBodyMassHighLighted = 1;
+                self.bodyMassNotificationTuple.isText = true;
+                self.bodyMassHighLightTuple.text = 1;
             }else{
                 sender.highlighted = false;
-                self.isBodyMassHighLighted = -1;
+                self.bodyMassNotificationTuple.isText = false;
+                self.bodyMassHighLightTuple.text = -1;
             }
         });
     }
     
-    @IBAction func OxySelect(sender: UIButton) {
+    @IBAction func oxyTextSelect(sender: UIButton) {
         dispatch_async(dispatch_get_main_queue(), {
-            if (self.isOxySelectHighLighted == -1){
+            if (self.oxyHighLightTuple.text == -1){
                 sender.highlighted = true;
-                self.isOxySelectHighLighted = 2;
+                self.oxyNotificaitonTuple.isText = true;
+                self.oxyHighLightTuple.text = 2;
             }else{
                 sender.highlighted = false;
-                self.isOxySelectHighLighted = -1;
+                self.oxyNotificaitonTuple.isText = false;
+                self.oxyHighLightTuple.text = -1;
             }
         });
     }
     
-    @IBAction func hrSelect(sender: UIButton) {
+    @IBAction func hrTextSelect(sender: UIButton) {
         dispatch_async(dispatch_get_main_queue(), {
-            if (self.isHRHighLighted == -1){
+            if (self.hRHighLightTuple.text == -1){
                 sender.highlighted = true;
-                self.isHRHighLighted = 3;
+                self.hrNotificaitionTuple.isText = true;
+                self.hRHighLightTuple.text = 3;
             }else{
                 sender.highlighted = false;
-                self.isHRHighLighted = -1;
+                self.hrNotificaitionTuple.isText = false
+                self.hRHighLightTuple.text = -1;
             }
         });
     }
     
+    //button action for enable / disable calls
+    
+    @IBAction func stepCallSelect(sender: UIButton) {
+        dispatch_async(dispatch_get_main_queue(), {
+            if (self.stepHighLightTuple.call == -1) {
+                sender.highlighted = true;
+                self.stepsNotificationTuple.isCall = true;
+                self.stepHighLightTuple.call = 0;
+            }else{
+                sender.highlighted = false;
+                self.stepsNotificationTuple.isCall = false;
+                self.stepHighLightTuple.call = -1;
+            }
+        });
+    }
+    
+    @IBAction func bodyMassCallSelect(sender: UIButton) {
+        dispatch_async(dispatch_get_main_queue(), {
+            if (self.bodyMassHighLightTuple.call == -1){
+                sender.highlighted = true;
+                self.bodyMassNotificationTuple.isCall = true;
+                self.bodyMassHighLightTuple.call = 1;
+            }else{
+                sender.highlighted = false;
+                self.bodyMassNotificationTuple.isCall = false;
+                self.bodyMassHighLightTuple.call = -1;
+            }
+        });
+        
+    }
+    
+    @IBAction func oxyCallSelect(sender: UIButton) {
+        dispatch_async(dispatch_get_main_queue(), {
+            if (self.oxyHighLightTuple.call == -1){
+                sender.highlighted = true;
+                self.oxyNotificaitonTuple.isCall = true;
+                self.oxyHighLightTuple.call = 2;
+            }else{
+                sender.highlighted = false;
+                self.oxyNotificaitonTuple.isCall = false;
+                self.oxyHighLightTuple.call = -1;
+            }
+        });
+    }
+    
+    @IBAction func hrCallSelect(sender: UIButton) {
+        dispatch_async(dispatch_get_main_queue(), {
+            if (self.hRHighLightTuple.call == -1){
+                sender.highlighted = true;
+                self.hrNotificaitionTuple.isCall = true;
+                self.hRHighLightTuple.call = 3;
+            }else{
+                sender.highlighted = false;
+                self.hrNotificaitionTuple.isCall = false
+                self.hRHighLightTuple.call = -1;
+            }
+        });
+        
+    }
+    
+    
+    // this is what happened when save is pressed
     
     func buttonAction(sender:UIButton!)
     {
         var patientId = UIDevice.currentDevice().identifierForVendor.UUIDString;
         var nameStr : String = nameField.text;
         var phoneNumberStr : String = phoneNumberField.text;
-        let statIds = ["7","1","6","9"];
-        var statIdSelections = [self.isStepHighLighted,self.isBodyMassHighLighted,self.isOxySelectHighLighted,self.isHRHighLighted];
+//        let statIds = ["7","1","6","9"];
+        var notifications = [self.stepHighLightTuple,self.bodyMassHighLightTuple,self.oxyHighLightTuple,self.hRHighLightTuple]
+        var statIdSelections = [self.stepsNotificationTuple,self.bodyMassNotificationTuple,self.oxyNotificaitonTuple,self.hrNotificaitionTuple];
         
         println(nameStr)
         println(phoneNumberStr)
         var repeat = "";
         
-        if contains(statIdSelections, 0) || contains(statIdSelections, 1)||contains(statIdSelections, 2)||contains(statIdSelections, 3){
+        // insert the contacts
         
-            for i in statIdSelections {
-                if( i != -1){
         
-                    var url = "http://colab-sbx-211.oit.duke.edu/PHPDatabaseCalls/contacts/insert.php?patient_id='\(patientId)'&contactName='\(nameStr)'&contactPhoneNumber='\(phoneNumberStr)'&stat_id=\(statIds[i])&repeat=\(repeat)";
-//                  println("dirty url:" + url);
+        if (self.checkSelected(self.stepHighLightTuple) || self.checkSelected(self.bodyMassHighLightTuple) || self.checkSelected(self.oxyHighLightTuple) || self.checkSelected(self.hRHighLightTuple)){
+        
+            for i in 0...notifications.count-1 {
+                if( self.checkSelected(notifications[i])){
+        
+                    var url = "http://colab-sbx-211.oit.duke.edu/PHPDatabaseCalls/contacts/insert.php?patient_id='\(patientId)'&contactName='\(nameStr)'&contactPhoneNumber='\(phoneNumberStr)'&stat_id='\(statIdSelections[i].statId)'&textsOn=\(statIdSelections[i].isText)&callsOn=\(statIdSelections[i].isCall)&repeat=\(repeat)";
+                    
+                    println("dirty url:" + url);
                     url = StringHelper.cleanURLString(url)
 //                  println("clean url:" + url);
                     makeHTTPRequest(url)
                     repeat = "true"
                 }
+                
+                
+                
             }
         }else{
             var url = "http://colab-sbx-211.oit.duke.edu/PHPDatabaseCalls/contacts/insert.php?patient_id='\(patientId)'&contactName='\(nameStr)'&contactPhoneNumber='\(phoneNumberStr)'";
@@ -113,6 +220,15 @@ class AddContactController : UIViewController {
             makeHTTPRequest(url)
         }
         
+        //update the notifications
+        
+    }
+    
+    func checkSelected(tuple : (text:Int,call:Int)) -> Bool{
+        if(tuple.text != -1 || tuple.call != -1){
+            return true;
+        }
+        return false;
     }
         
     func makeHTTPRequest(urlStringWithParameters : String) {
